cc0350eee13e0ac61964d7938834dabec381e938
Kera Cudmore <kera.cudmore@hotmail.com>
2022-02-01 20:35:40 +0000

    added favicon and tidy up of code for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,11 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        checkAnswer();
+    }
+})
 
     runGame("addition");
 })
@@ -24,6 +29,9 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed.
  */
 function runGame(gameType) {
+
+document.getElementById("answer-box").value = "";
+document.getElementById("answer-box").focus();
 
     // Creates two randomnumbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
